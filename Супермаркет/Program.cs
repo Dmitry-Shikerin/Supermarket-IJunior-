4ed6c8b4c65fc00ddb8c14cf12aff670a9ef5715
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-08-06 20:59:23 +0300

    Добавьте файлы проекта.

---
+++ Супермаркет/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Супермаркет
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int buyersNumber = 6;
+
+            int minMoney = 150;
+            int maxMoney = 450;
+            int randomMoney = Utils.GetRandomValue(minMoney, maxMoney);
+
+            Queue<Buyer> buyers = new Queue<Buyer>(new List<Buyer>()
+            {
+                
+            
+
+            
+
+                new Buyer(randomMoney),
+                new Buyer(randomMoney),
+                new Buyer(randomMoney),
+                new Buyer(randomMoney),
+                new Buyer(randomMoney)
+            );
+
+            //for (int i = 0; i < buyersNumber; i++)
+            //{
+            //    new Buyer(randomMoney);
+            //}
+
+                List<Product> products = new List<Product>()
+            {
+                new Product("Банан", 25),
+                new Product("Яблоко", 35),
+                new Product("Груша", 27),
+                new Product("Макароны", 68),
+                new Product("Хлеб", 38),
+                new Product("Молоко", 78),
+                new Product("Говядина", 170),
+                new Product("Чипсы", 50),
+                new Product("Пиво", 97),
+                new Product("Хот-Дог", 45)
+            };
+
+            int bayersNumber = 0;
+
+            Supermarket supermarket = new Supermarket(products);
+
+            while (buyers.Count > 0)
+            {
+                Buyer buyer = buyers.Dequeue();
+
+                bayersNumber++;
+
+                Console.WriteLine($"Покупатель {bayersNumber}");
+
+                supermarket.Serve(buyer);
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    class Buyer
+    {
+        private List<Product> _products = new List<Product>();
+
+        private int _money;
+
+        public Buyer(int money)
+        {
+            _money = money;
+        }
+
+        public void GetProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public bool CanPay(int sumAllProduct)
+        {
+            return sumAllProduct <= _money;
+        }
+
+        public int PayForProducts(int sumProducts)
+        {
+            _money -= sumProducts;
+
+            return sumProducts;
+        }
+    }
+
+    class Cart
+    {
+        private List<Product> _products;
+
+        public Cart(List<Product> products)
+        {
+            _products = products;
+        }
+
+        public int CalculateProductsPrice()
+        {
+            int productsSum = 0;
+
+            foreach (Product product in _products)
+            {
+                productsSum += product.Price;
+            }
+
+            return productsSum;
+        }
+
+        public void DeleteRandomProduct()
+        {
+            int randomProduct = Utils.GetRandomValue(_products.Count);
+
+            Console.WriteLine($"Нехватило денег выложен Продукт {_products[randomProduct].Name} с ценой {_products[randomProduct].Price}");
+
+            _products.RemoveAt(randomProduct);
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; }
+        public int Price { get; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Price}";
+        }
+    }
+
+    class Supermarket
+    {
+        private List<Product> _products;
+
+        private int _money = 0;
+
+        public Supermarket(List<Product> products)
+        {
+            _products = products;
+        }
+
+
+        public void Serve(Buyer buyer)
+        {
+            List<Product> products = CreateCart();
+
+            Cart cart = new Cart(products);
+
+            while (buyer.CanPay(cart.CalculateProductsPrice()) == false)
+            {
+                cart.DeleteRandomProduct();
+            }
+
+            _money += cart.CalculateProductsPrice();
+
+            Console.Write("Куплено: ");
+
+            foreach (Product product in products)
+            {
+                Console.Write($"{product.Name} {product.Price},");
+            }
+
+            Console.WriteLine($"\nСумма покупок {cart.CalculateProductsPrice()}");
+            Console.WriteLine($"Выручка магазина {_money}"); ;
+        }
+
+        private List<Product> CreateCart()
+        {
+            List<Product> products = new List<Product>();
+
+            int minProductNumber = 3;
+            int maxProductNumber = 10;
+            int randomProductsCount = Utils.GetRandomValue(minProductNumber, maxProductNumber);
+
+            for (int i = 0; i < randomProductsCount; i++)
+            {
+                products.Add(_products[Utils.GetRandomValue(_products.Count)]);
+            }
+
+            return products;
+        }
+    }
+
+    public static class Utils
+    {
+        private static Random _random = new Random();
+
+        public static int GetRandomValue(int minValue, int maxValue)
+        {
+            return _random.Next(minValue, maxValue);
+        }
+
+        public static int GetRandomValue(int maxValue)
+        {
+            return _random.Next(maxValue);
+        }
+    }
+}
